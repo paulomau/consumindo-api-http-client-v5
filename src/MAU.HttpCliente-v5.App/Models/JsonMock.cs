d5a272afcc8081396cc0030dd9ea03244098cc91
Paulo Maurício <paulomau@hotmail.com>
2022-05-20 02:35:30 -0300

    Exemplo de código HttpClient v5

---
+++ src/MAU.HttpCliente-v5.App/Models/JsonMock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MAU.HttpCliente.App.Models
+{
+  public class Data
+  {
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("author")]
+    public string Author { get; set; }
+
+    [JsonPropertyName("num_comments")]
+    public int? NumComments { get; set; }
+
+    [JsonPropertyName("story_id")]
+    public object StoryId { get; set; }
+
+    [JsonPropertyName("story_title")]
+    public string StoryTitle { get; set; }
+
+    [JsonPropertyName("story_url")]
+    public string StoryUrl { get; set; }
+
+    [JsonPropertyName("parent_id")]
+    public int? ParentId { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public int CreatedAt { get; set; }
+  }
+
+  public class JsonRoot
+  {
+    [JsonPropertyName("page")]
+    public int Page { get; set; }
+
+    [JsonPropertyName("per_page")]
+    public int PerPage { get; set; }
+
+    [JsonPropertyName("total")]
+    public int Total { get; set; }
+
+    [JsonPropertyName("total_pages")]
+    public int TotalPages { get; set; }
+
+    [JsonPropertyName("data")]
+    public List<Data> Data { get; set; }
+  }
+}
---
+++ src/MAU.HttpCliente-v5.App/Program.cs
@@ -0,0 +1,64 @@
+﻿using MAU.HttpCliente.App.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace MAU.HttpCliente_v5.App
+{
+  internal class Program
+  {
+    static async Task Main(string[] args)
+    {
+      WriteLine("----------------------------");
+      WriteLine("     HackerRank - Autores");
+      WriteLine("-----------------------------");
+      Write("Entre com o nome de um autor: ");
+      var autor = ReadLine();
+
+      // "https://jsonmock.hackerrank.com/api/article_users";
+      string uri = "https://jsonmock.hackerrank.com/api/articles";
+      var cliente = new HttpClient();
+
+      var totalPages = 1;
+      List<string> titles = new();
+
+      for (int i = 1; i <= totalPages; i++)
+      {
+        JsonRoot resultado = await cliente.GetFromJsonAsync<JsonRoot>($"{uri}?author={autor}&page={i}");
+
+        var paginas = resultado.TotalPages;
+
+        WriteLine($"Página atual: {resultado.Page}");
+        WriteLine($"Registros por página: {resultado.PerPage}");
+        WriteLine($"Registros totais: {resultado.Total}");
+        WriteLine($"Total de Páginas: {paginas}");
+
+        foreach (var item in resultado.Data)
+        {
+          if (!string.IsNullOrEmpty(item.Title))
+          {
+            titles.Add(item.Title);
+          }
+          else if (!string.IsNullOrEmpty(item.StoryTitle))
+          {
+            titles.Add(item.StoryTitle);
+          }
+        }
+        if (paginas > 1) totalPages = paginas;
+      }
+
+      WriteLine("");
+      WriteLine("----------------------------");
+      WriteLine("    Lista de títulos:");
+      WriteLine("----------------------------");
+      foreach (var item in titles)
+      {
+        WriteLine(item);
+      }
+      ReadKey();
+    }
+  }
+}
